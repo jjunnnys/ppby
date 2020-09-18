6467a0527e066d9d10ea401ea6977fb5a5181803
ppby <salswnss@gmail.com>
2020-09-18 15:23:23 +0900

    #1.1 Securing the Keys

--- src/firebase.js
+++ src/firebase.js
@@ -1,16 +1,13 @@
 import * as firebase from 'firebase/app';
-import dotenv from 'dotenv';
-
-dotenv.config();
 
 const firebaseConfig = {
-  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-  authDomain: 'ppby-sns.firebaseapp.com',
-  databaseURL: 'https://ppby-sns.firebaseio.com',
-  projectId: 'ppby-sns',
-  storageBucket: 'ppby-sns.appspot.com',
-  messagingSenderId: '493510244551',
-  appId: '1:493510244551:web:decae920d2a67b52c4a6e2',
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_DATABASE_URL,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID,
 };
 
 export default firebase.initializeApp(firebaseConfig);
