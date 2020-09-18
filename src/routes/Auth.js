6a80ef2b18bf7f0264620beb80ef86bb44e180df
ppby <salswnss@gmail.com>
2020-09-18 16:52:43 +0900

    #2.1 Login Form part One

--- src/routes/Auth.js
+++ src/routes/Auth.js
@@ -1,7 +1,52 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const Auth = () => {
-  return <div>인증</div>;
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const onChange = (e) => {
+    const {
+      target: { name, value },
+    } = e;
+
+    if (name === 'email') {
+      setEmail(value);
+    } else if (name === 'password') {
+      setPassword(value);
+    }
+  };
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+  };
+
+  return (
+    <>
+      <form onSubmit={onSubmit}>
+        <input
+          name="email"
+          type="text"
+          placeholder="이메일"
+          value={email}
+          onChange={onChange}
+          required
+        />
+        <input
+          name="password"
+          type="text"
+          placeholder="비밀번호"
+          value={password}
+          onChange={onChange}
+          required
+        />
+        <input type="submit" value="로그인" />
+      </form>
+      <div>
+        <button>google 로그인</button>
+        <button>github 로그인</button>
+      </div>
+    </>
+  );
 };
 
 export default Auth;
