9cdee6831c1ca386bf7cf7fa16c381a00f28b361
ppby <salswnss@gmail.com>
2020-09-18 15:43:21 +0900

    #1.2 Router Setup

--- src/App.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import firebase from './firebase';
-
-function App() {
-  console.log(firebase);
-  return <div></div>;
-}
-
-export default App;
---
+++ src/routes/EditProfile.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const EditProfile = () => {
+  return <div>프로필 수정</div>;
+};
+
+export default EditProfile;
