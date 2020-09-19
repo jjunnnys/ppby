4866cc3ebd6124f3f98dc9f020ae7c222ae62070
ppby <salswnss@gmail.com>
2020-09-19 16:53:22 +0900

    #3.0 Form and Databse Setup

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,7 +1,28 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const Home = () => {
-  return <div>홈</div>;
+  const [ppby, setPpby] = useState('');
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+  };
+
+  const onChange = (e) => {
+    setPpby(() => e.target.value);
+  };
+
+  return (
+    <form onSubmit={onSubmit}>
+      <input
+        type="text"
+        placeholder="무슨 생각을 하시나요?"
+        maxLength={120}
+        value={ppby}
+        onChange={onChange}
+      />
+      <input type="submit" value="PPBY" />
+    </form>
+  );
 };
 
 export default Home;
