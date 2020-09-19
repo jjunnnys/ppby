2f1cca33a25b06998f81daeb8604927dca461998
ppby <salswnss@gmail.com>
2020-09-19 22:03:57 +0900

    #3.5 Delete and Update part Two

--- src/components/Ppby.js
+++ src/components/Ppby.js
@@ -1,13 +1,69 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { dbService } from '../fbApp';
 
 const Ppby = ({ ppby, isOwner }) => {
+  const [checkEdit, setCheckEdit] = useState(false);
+  const [editing, setEditing] = useState(ppby.text);
+
+  const onDelete = async () => {
+    const ok = window.confirm('정말로 삭제 하시겠습니까?');
+
+    if (ok) {
+      await dbService.doc(`ppbys/${ppby.id}`).delete();
+    }
+  };
+
+  const toggleEditing = () => setCheckEdit((prev) => !prev);
+
+  const onSubmit = async (e) => {
+    e.preventDefault();
+
+    await dbService.doc(`ppbys/${ppby.id}`).update({
+      text: editing,
+    });
+
+    setCheckEdit((prev) => !prev);
+  };
+
+  const onChange = (e) => {
+    const {
+      target: { value },
+    } = e;
+
+    setEditing(() => value);
+  };
+
   return (
     <div>
-      <h4>{ppby.text}</h4>
-      {isOwner && (
+      {checkEdit ? (
         <>
-          <button type="button">삭제</button>
-          <button type="button">수정</button>
+          <form onSubmit={onSubmit}>
+            <input
+              type="text"
+              placeholder="수정할 글 작성"
+              value={editing}
+              onChange={onChange}
+              required
+            />
+            <input type="submit" />
+          </form>
+          <button type="button" onClick={toggleEditing}>
+            취소
+          </button>
+        </>
+      ) : (
+        <>
+          <h4>{ppby.text}</h4>
+          {isOwner && (
+            <>
+              <button type="button" onClick={onDelete}>
+                삭제
+              </button>
+              <button type="button" onClick={toggleEditing}>
+                수정
+              </button>
+            </>
+          )}
         </>
       )}
     </div>
